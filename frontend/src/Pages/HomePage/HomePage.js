3471fa6dac87785551ff6e128b47980f1f833055
rohangawhade <gawhaderohan@gmail.com>
2021-05-10 13:04:46 +0530

    Added sidebar and routes

---
+++ frontend/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <div>
+            <h2>Homepage</h2>
+        </div>
+    )
+}
---
+++ frontend/src/Pages/Model/Model.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Model() {
+    return (
+        <div>
+            <h2>Model</h2>
+        </div>
+    )
+}

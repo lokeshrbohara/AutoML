25ff4f7175da3dea0157479e302eb9c9eabe2271
rohangawhade <gawhaderohan@gmail.com>
2021-08-28 17:53:34 +0530

    Successfully storing uploaded zip file

--- frontend/src/Pages/DataPreprocess/DataPreprocess.js
+++ frontend/src/Pages/DataPreprocess/DataPreprocess.js
@@ -10,12 +10,14 @@ export default function DataPreprocess() {
             authorization: 'authorization-text',
         },
         onChange(info) {
+            console.log(info.file.status);
             if (info.file.status !== 'uploading') {
                 console.log(info.file, info.fileList);
             }
             if (info.file.status === 'done') {
                 message.success(`${info.file.name} file uploaded successfully`);
             } else if (info.file.status === 'error') {
+                console.log("NOOOOOOOOOOOOOOOOO");
                 message.error(`${info.file.name} file upload failed.`);
             }
         },
